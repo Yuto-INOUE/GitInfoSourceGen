b103f34c0b3e9cea075c12d38f93975d6ce3935f
Yuto Inoue <dsyuuto@gmail.com>
2023-01-25 23:36:59 +0900

    completed

---
+++ SourceGenConsoleTest/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using SourceGen;
+
+namespace SourceGenConsoleTest;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		Console.WriteLine(HogeMoge.BranchName);
+	}
+}
+
+[GenerateGitInformation]
+public partial class HogeMoge
+{
+}
